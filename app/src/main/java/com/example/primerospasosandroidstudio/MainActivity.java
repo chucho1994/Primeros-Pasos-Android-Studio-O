69fb27ea9d2afcdfa3b6e4d38cf0e23e936f791b
chucho1994 <51958012+chucho1994@users.noreply.github.com>
2019-07-14 21:30:18 -0500

    Sumar Restas usando radioGroup y radioButton y que de a escojer que accion queremos

--- app/src/main/java/com/example/primerospasosandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/primerospasosandroidstudio/MainActivity.java
@@ -1,9 +1,8 @@
 
 
-/*Hacer una app que contenga un texView donde nos muestre
-la evaluacion de las calificaciones de fisica, matematicas, español y nos diga
-si el alumno aprobo o rebrobo mediante el texView que lo va a mandar a llamar el
-boton evaluar
+/*Hacer una app que ingrese dos valores text Number y luego que contenga dos bptones
+una para sumar y otro para restar y uno tercero que nos permita hacer la accion que
+seleccionamos y nos muestre los resultados en un  text View
  */
 
 package com.example.primerospasosandroidstudio;
@@ -13,12 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast; //nos aparece asi por que no la estamos utilizando la podemos
 //si queremos  y las que si se usan aparacen bien marcadas
 
-import android.widget.Toast;//libreria para utilizar clase toast-
-//         /\se pone automaticamente cuando se agrega la clase si no lo poneemos manual
 
 
 
@@ -26,13 +24,17 @@ import android.widget.Toast;//libreria para utilizar clase toast-
 public class MainActivity extends AppCompatActivity {
 
     //Lo primero que se hace es agregar objetos de tipo EditText en modo privado
-    //para que nadamas esta clase tenga acceso a ellos y van a hacer  4   tres para que el alumno
-    //ingrese la calificacion de matematicas, fisica y español y uno para promedio
+    //para que nadamas esta clase tenga acceso a ellos y van a hacer  3   dos para que ingrese valor 1 y
+    // valor 2 y uno tercero que muestre el resultado
 
+    //       \/objetos que nos permita utilizar los texView y edit Text etc para poder colocar valores
+                         //dentro de nuestros controles .
     private EditText et1;
     private EditText et2;
-    private EditText et3;
+
     private TextView tv1;
+
+    private RadioButton rb1,rb2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,20 +43,13 @@ public class MainActivity extends AppCompatActivity {
         //Ejemplo sin proposito:
 
 
-    /*Nota uno haciendo Casting
+/*
 
        \/Objeto de tipo
         et1=(EditText)findViewById();
                /\Castin para que nuestro primer valor que va a ingresar el usuario que es textView
                 lo convierta a EditText y asi poder meter el valor dentro del objeto EditTextx et1
                 y asi se compatible     */
-
-
-        int matematicas = 0;
-        int física= 0;
-        int español=0;
-        int promedio = 0;
-
 
 
 
@@ -78,16 +73,7 @@ public class MainActivity extends AppCompatActivity {
                             Sin la clase R no se puede hacer la comunicacion entre la parte
                             grafica y logica
 
-            Toast.makeText(this, "Aprovado", Toast.LENGTH_SHORT).show();
-            //metodo para mostarar textos, imagenes, videos ect dependiendo de como lo
-            //configuremos
 
-        } else if (promedio <= 5) {
-                                                          //       \/metodo para dar cuatno tiempo sea visible
-            Toast.makeText(this, "Rebrobado", Toast.LENGTH_SHORT).show();
-                                          // /\Texto que aparecera en la pantalla /\Eeste metodo nos
-            //hace visible que nuestor mensaje se muestre en la pantalla del dispositivo
-        }
 
 
                              et1=(EditText)findViewById(R.id.txt_num1);
@@ -100,58 +86,47 @@ public class MainActivity extends AppCompatActivity {
 
         et1 = (EditText) findViewById(R.id.txt_num1);
        et2 = (EditText) findViewById(R.id.txt_num2);
-       et3=(EditText)findViewById(R.id.txt_num3);
+
 
      tv1 = (TextView) findViewById(R.id.txt_Resultado);
+
+        rb1 = (RadioButton) findViewById(R.id.radioButton1);
+        rb2 = (RadioButton) findViewById(R.id.radioButton2);
 
 
     }
     //El metodo se va a mandar a llamar cuando demos click en el main activity de Evaluar
     // el metodo se agrega en   common attributes y luego en onclick y seleccionar metodo
     //El siguiente metodo realiza la Evaluacion
-    //           \/Retornara valores nuestro metodo
-   public void Evaluar(View view) {
+    //           \/Retornara valores nuestro metodo que son los resultados
+   public void sumar_o_restar(View view) {
 
        //       \/variable de tipo stiring que guardara
        //                  \/Metodo que nos permite recuperar todoo el edit text
        String valor1 = et1.getText().toString();
        String valor2 = et2.getText().toString();
-       String valor3 = et3.getText().toString();
+
        //                             /\metodo que nos permite parsear a string
        //                           \/Parseo de String a entero(int)
        int numero1 = Integer.parseInt(valor1);
        int numero2 = Integer.parseInt(valor2);
-       int numero3 = Integer.parseInt(valor3);
 
-       int promedio= (numero1 + numero2+numero3)/3;
 
-        /*                      \/Parsear de entero a String forma novata
-        String resultado1=suma+" ";
+       int sumar_1= numero1+numero2;
 
-        */
-       //Parsear de entero a string de manera pro
+         if(rb1.isChecked()==true){
 
-     //  String resultado1 = String.valueOf(promedio);
-       //tv1.setText(resultado1);
+             String resultado_Total=String.valueOf(sumar_1);
+             tv1.setText(resultado_Total);
 
-       String mensaje="Alumno aprobado con: ";
-       String mensaje2="Alumno Rebrobado con ";
-
-       if(promedio>=6){
-           tv1.setText(mensaje+promedio);
-       }if(promedio<=5){
-           tv1.setText(mensaje2+promedio);
-       }
+         }else if(rb2.isChecked()==true){
+             int resta=numero1-numero2;
+             String res_resultado=String.valueOf(resta);
+             tv1.setText(res_resultado);
+         }
 
 
 
-       //para hacer texto grade y chico de editor de texto nos vamos a settings /keymap y en buscar
-       //le damos en font size y seleccionamos para hacer grande o chico y lo configuramos
-       //con el teclado o raton a nuestro gusto
-
-
-       //hola estamos checando la rama pruebas_calculadora que esta dentro de la ramma
-       //principal master la vamos a subir al git hub
 
    }
 
